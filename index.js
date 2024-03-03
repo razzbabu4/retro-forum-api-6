495ecf19106951f863155dd6443545ef85cc825e
Tareq <razzbabut@gmail.com>
2024-03-04 01:24:22 +0600

    retrieve data from api by category search

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const latestPost = document.getElementById('latest-post');
 
 // all post
 const loadAllPost = async() => {
+    postContainer.textContent = '';
+
     const response = await fetch('https://openapi.programming-hero.com/api/retro-forum/posts');
     const data = await response.json();
     const allPost = data.posts;
@@ -43,7 +45,7 @@ const loadAllPost = async() => {
                         <p><i class="fa-regular fa-eye mr-2"></i><span>${post.view_count}</span></p>
                         <p><i class="fa-regular fa-clock mr-2"></i><span>${post.posted_time}</span></p>
                     </div>
-                    <div onclick="titleView('${post.title}', '${post.view_count}')" class="h-8 w-8 rounded-full bg-green-600 text-center p-1">
+                    <div onclick="titleView('${post.title}', '${post.view_count}')" class="h-8 w-8 rounded-full bg-green-600 text-center p-1 cursor-pointer">
                         <i class="fa-regular fa-envelope-open text-white"></i>
                     </div>
                 </div>
@@ -72,7 +74,7 @@ const loadLatestPost = async() => {
     const response = await fetch('https://openapi.programming-hero.com/api/retro-forum/latest-posts');
     const data = await response.json();
     data.forEach((newPost) => {
-        console.log(newPost);
+        // console.log(newPost);
 
         const div = document.createElement('div');
         div.classList = `border-2 p-4 lg:p-8 rounded-xl space-y-5`;
@@ -93,6 +95,68 @@ const loadLatestPost = async() => {
         latestPost.appendChild(div);
 
     })
+}
+
+const searchPost = async(searchText) => {
+    const response = await fetch(`https://openapi.programming-hero.com/api/retro-forum/posts?category=${searchText}`);
+    const data = await response.json();
+    const categoryPost = data.posts;
+    // console.log(categoryPost);
+    postContainer.textContent = ''
+
+    categoryPost.forEach((post) => {
+        // console.log(post);    
+        let activeStatus ='';
+
+        if(post.isActive){
+            activeStatus = `bg-green-600`;
+        }
+        else{
+            activeStatus = `bg-red-600`;
+        }
+
+        const div = document.createElement('div');
+        div.classList = `p-4 lg:p-6 bg-[#797DFC1A] flex flex-col lg:flex-row gap-2 lg:gap-5 rounded-lg`;
+        div.innerHTML = `
+            <!-- img left-->
+        <div class="relative w-10">
+            <img class="rounded-xl h-10 w-10" src="${post.image}" alt="">
+                <div class="absolute h-3 w-3 rounded-full ${activeStatus}  border-2 border-white -top-1 -right-1"></div>
+        </div>
+            <!-- info right-->
+            <div class="space-y-4 w-full">
+                <div class="flex justify-between lg:justify-normal gap-2 lg:gap-5">
+                    <p>#${post.category}</p>
+                    <p>Author: ${post.author.name}</p>
+                </div>
+                <h1 class="font-bold">'${post.title}'</h1>
+                <p class="">${post.description}</p>
+
+                <hr class="my-2 h-0.5 bg-gray-300">
+
+                <div class="flex justify-between items-center">
+                    <div class="flex gap-2 lg:gap-8">
+                        <p><i class="fa-regular fa-message mr-2"></i><span>${post.comment_count}</span></p>
+                        <p><i class="fa-regular fa-eye mr-2"></i><span>${post.view_count}</span></p>
+                        <p><i class="fa-regular fa-clock mr-2"></i><span>${post.posted_time}</span></p>
+                    </div>
+                    <div onclick="titleView('${post.title}', '${post.view_count}')" class="h-8 w-8 rounded-full bg-green-600 text-center p-1 cursor-pointer">
+                        <i class="fa-regular fa-envelope-open text-white"></i>
+                    </div>
+                </div>
+            </div>
+        `;
+       
+        postContainer.appendChild(div);  
+    });
+}
+
+const searchCategoryPost = () => {
+    const discussionSection = document.getElementById('discuss-section');
+    const searchValue = document.getElementById('Search-text').value;
+    searchPost(searchValue);
+    document.getElementById('Search-text').value = ""
+    discussionSection.scrollIntoView({behavior: "smooth"});
 }
 
 loadLatestPost();
